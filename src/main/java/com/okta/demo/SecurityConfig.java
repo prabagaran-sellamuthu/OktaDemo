da647b8c8be131cb95bfaf9fafa886065ae9ed40
Prabagaran Sellamuthu <38026104+prabagaran-sellamuthu@users.noreply.github.com>
2024-10-22 00:56:59 +0100

    Update SecurityConfig.java

--- src/main/java/com/okta/demo/SecurityConfig.java
+++ src/main/java/com/okta/demo/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.okta.demo;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,19 +9,15 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity  // Enables method-level security annotations like @PreAuthorize
 public class SecurityConfig {
-
     @Bean
     public JwtDecoder jwtDecoder() {
         String jwkSetUri = "https://dev-07913094.okta.com/oauth2/default/v1/keys";  // Replace with your Okta JWK Set URI
         return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -31,7 +26,6 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests
-                    .requestMatchers("/logout", "/oidc/logout").permitAll() 
                     .anyRequest().authenticated()
             )
             .oauth2Login(oauth2 ->
@@ -40,19 +34,12 @@ public class SecurityConfig {
                         userInfo
                             .oidcUserService(oidcUserService()) 
                     )
-            ).oidcLogout((logout) -> logout
-            .backChannel(Customizer.withDefaults())
-        );
+            );
     
         return http.build();
     }
-  
     @Bean
     public OidcUserService oidcUserService() {
         return new OidcUserService();
-    }
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher() {
-        return new HttpSessionEventPublisher();
     }
 }
